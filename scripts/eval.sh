277548a62aa4fdfe09c4496b07296d1549e72f94
KinGeorge <dxleec@skl-a-53.rc.rit.edu>
2024-03-30 10:48:38 -0400

    scripts

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,9 +1,9 @@
 # Evaluating LLaVA on multiple datasets
 accelerate launch --num_processes=4 -m lmms_eval \
     --model llava   \
-    --model_args pretrained="llava-hf/llava-v1.6-vicuna-7b-hf"  \
-    --tasks mme,docvqa_chartqa \
+    --model_args pretrained="liuhaotian/llava-v1.5-7b"  \
+    --tasks ai2d,chartqa,docvqa,gqa,mme,mmbench,mmmu,mmvet,pope,seedbench,scienceqa_img,vizwiz_vqa,vqav2 \
     --batch_size 1 \
     --log_samples \
-    --log_samples_suffix llava_v1.5_mme_docvqa_chartqa \
+    --log_samples_suffix llava_v1.5_all_in_one \
     --output_path ./logs/
